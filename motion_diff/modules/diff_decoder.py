ba18515bb6a0ed68d2243e13fed56c3b4ce80102
zyHo_0x1 <57285621+lionel-me@users.noreply.github.com>
2024-10-18 11:49:06 +0800

    Update

--- motion_diff/modules/diff_decoder.py
+++ motion_diff/modules/diff_decoder.py
@@ -135,7 +135,7 @@ class DiffDecoder(nn.Module):
                     head_dim=self.head_dim,
                     dropout=self.dropout,
                     bipartite=False,
-                    has_pos_emb=True,
+                    has_pos_emb=False,
                 )
                 for _ in range(self.num_layers)
             ]
